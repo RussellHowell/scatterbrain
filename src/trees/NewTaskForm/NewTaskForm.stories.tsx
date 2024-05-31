210658f547f60f856a20a77e2949a8bbc1cba96b
Russell Howell <russell.howell@spectralogic.com>
2024-05-30 21:27:52 -0600

    NewTaskForm

---
+++ src/trees/NewTaskForm/NewTaskForm.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { NewTaskForm } from "./NewTaskForm";
+import { Center } from "@chakra-ui/react";
+
+const meta: Meta = {
+  title: 'Component Trees/New Task Form',
+  render: () => {
+    return (
+      <Center>
+        <NewTaskForm spacing={ 30 } alignItems='center'/>
+      </Center>
+    )
+  } 
+}
+
+export default meta;
+
+export const NewTaskFormStory: StoryObj = {};
---
+++ src/trees/NewTaskForm/NewTaskForm.tsx
@@ -0,0 +1,48 @@
+import { Button, Radio, RadioGroup, Stack, StackProps, Textarea } from "@chakra-ui/react";
+import { Controller, useForm } from "react-hook-form";
+import { Task, TaskSize } from "../../common/types";
+
+type NewTaskFormProps = StackProps;
+
+export const NewTaskForm = ({ ...styleProps }: NewTaskFormProps) => {
+  const { control, formState: { isValid } } = useForm<Task>();
+
+  return (
+    <Stack { ...styleProps }>
+      <Controller
+        name="content"
+        rules={{ required: true }}
+        control={ control }
+        render={ ({ field }) => (
+          <Textarea 
+            size="lg"
+            placeholder="What do you have to do?"
+            variant="unstyled"
+            { ...field }
+            />
+        ) }
+      />
+      <Controller 
+        name="size"
+        rules={{ required: true }}
+        control={ control }
+        render={ ({ field }) => (
+          <RadioGroup { ...field }>
+            <Stack direction='row'>
+              {
+                (['small', 'medium', 'large'] satisfies TaskSize[]).map(size => (
+                  <Radio key={ size } value={ size }>{ size }</Radio>
+                ))
+              }
+            </Stack>
+          </RadioGroup>
+        )} 
+      />
+      <Button
+        isDisabled={ !isValid }
+        variant='outline'>
+          Add Task
+      </Button>
+    </Stack>
+  )
+}
